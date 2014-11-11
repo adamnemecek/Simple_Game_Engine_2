b12b7e9c9c94ad06ce3067a46bc15894297d735d
amdreallyfast <amdreallyfast@hotmail.com>
2014-11-11 09:53:16 -0800

    Set the Engine_Tester project to run all tests.  I have none written, but the framework is now in place.

--- Sandbox_Game_2/Engine_Tester/main.cpp
+++ Sandbox_Game_2/Engine_Tester/main.cpp
@@ -1,6 +1,9 @@
-
+#include <gtest\gtest.h>
 
 int main(int argc, char **argv)
 {
-   return 0;
+   // initialize the google test suit from the top level namespace
+   // Note: To run these tests, this project must be the startup project.
+   ::testing::InitGoogleTest(&argc, argv);
+   RUN_ALL_TESTS();
 }
