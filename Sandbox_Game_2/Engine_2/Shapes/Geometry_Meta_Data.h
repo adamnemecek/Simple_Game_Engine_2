f6563a28dc0a38916c0a64fe5803f094d9b6e28b
amdreallyfast <amdreallyfast@hotmail.com>
2014-12-24 09:28:20 -0700

    Made the Geometry_Meta_Data class into a struct.  This simplifies the data structure, but also opens it up to misuse.  I'll trust that I'll be nice to the data.  This struct will be a private member of the Geometry class, so at least it will be protected that way.

--- Sandbox_Game_2/Engine_2/Shapes/Geometry_Meta_Data.h
+++ Sandbox_Game_2/Engine_2/Shapes/Geometry_Meta_Data.h
@@ -3,35 +3,12 @@
 
 #include <glm\vec3.hpp>
 
-// forward declarations
-namespace Entities
-{
-   class AABB_Component;
-   class Physics_Component;
-}
-
 namespace Shapes
 {
-   namespace Geometry_Creation
+   // it's all public, so be nice to the data
+   // Note: The geometry loader is responsible for analyzing and setting this info.
+   struct __declspec(dllexport) Geometry_Meta_Data
    {
-      class Geometry_Loader;
-   }
-}
-
-namespace Shapes
-{
-
-   class __declspec(dllexport) Geometry_Meta_Data
-   {
-   public:
-      // default constructors and destructor
-
-   private:
-      // let a select few classes have access to these
-      friend class Geometry_Creation::Geometry_Loader;
-      friend class Entities::AABB_Component;
-      friend class Entities::Physics_Component;
-
       float m_min_X;
       float m_max_X;
       float m_min_Y;
