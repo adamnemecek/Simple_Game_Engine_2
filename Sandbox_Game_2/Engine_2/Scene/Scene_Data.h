e1591554ae98a6b10c6b5af1bc59849c2354667a
amdreallyfast <amdreallyfast@hotmail.com>
2015-02-13 16:45:24 -0700

    Decided to migrate the Scene_Loader class' functionality into Scene_Data itself (perhaps I will rename it back to simply 'Scene').  I also started adding explicit entity-geometry pairings to make the scene saving much easier.  I also decided to store the renderer inside the Scene_Data class on the grounds that having a scene necessitates having a renderer, and having a scene die necessitates that all the renderables also die.

--- Sandbox_Game_2/Engine_2/Scene/Scene_Data.h
+++ Sandbox_Game_2/Engine_2/Scene/Scene_Data.h
@@ -5,6 +5,14 @@
 #include <string>
 #include <Entities\Entity.h>
 #include <Shapes\Geometry.h>
+#include <Rendering\Renderer.h>
+
+// forward declaration
+namespace Rendering
+{
+   class Renderer;
+}
+
 
 namespace Scene
 {
@@ -13,8 +21,15 @@ namespace Scene
    public:
       bool initialize();
 
+      bool load(const std::string& file_path);
+      bool save(const std::string& file_path);
+
+      void draw_scene();
+
       Entities::Entity *new_entity(const std::string& new_entity_id);
+
       Shapes::Geometry *new_geometry(const Shapes::Shape_Data& new_shape_data);
+      void new_entity_geometry_pairing(const Entities::Entity *entity_ptr, const Shapes::Geometry *geo_ptr);
 
       // checks if any of the already-loaded geometries have a shape that matches
       // the argument id string, which is simply a mashing of all the parameters
@@ -24,10 +39,22 @@ namespace Scene
       Shapes::Geometry *geometry_already_loaded(const std::string& shape_id_str);
 
    private:
+      // keep this around so that new renderables can be added as new entities and
+      // geometries are introduced
+      // Note: I am keeping this as a class member instead of a pointer because it
+      // only needs to live as long as the scene.  If there is no scene, then there
+      // is no need of a renderer.  If there is a scene, then there only needs to be 
+      // a single renderer with its collection of renderables.  When the scene object
+      // dies, then all renderables should also die.
+      // 
+      // Making the renderer a class member accomplishes this goal.
+      Rendering::Renderer m_renderer;
+
       // these are vectors of classes instead of pointers because I want the
       // class' destructors to be called when the scene instance dies
       std::vector<Entities::Entity> m_entities;
       std::vector<Shapes::Geometry> m_geometries;
+      std::vector<std::pair<const Entities::Entity *, const Shapes::Geometry *>> m_entity_geometry_pairings;
    };
 }
 
